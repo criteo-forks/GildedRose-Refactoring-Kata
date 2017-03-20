f7eee8d13c91cabecf1e87930ce5041c03f511ae
Sergei Lebedev <s.lebedev@criteo.com>
2017-03-20 11:19:07 +0100

    Switched to py.test in the Python version

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-import unittest
+"""
+Do not run this file directly, use ``py.test`` instead.
+"""
+
 
 from gilded_rose import Item, GildedRose
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "fixme"
